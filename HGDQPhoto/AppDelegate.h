6f2ae0a2d2d6ffa8c97dae358b66d5c2013376d9
HGDQ <zhuming3834@sina.com>
2017-01-04 09:40:21 +0800

    Initial Commit

---
+++ HGDQPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HGDQPhoto
+//
+//  Created by hgdq on 2017/1/4.
+//  Copyright © 2017年 hgdq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HGDQPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HGDQPhoto
+//
+//  Created by hgdq on 2017/1/4.
+//  Copyright © 2017年 hgdq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
